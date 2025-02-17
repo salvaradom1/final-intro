e94bc92f13993bc003efba13df6a5bc77ae43a94
FranciscoSegura-fiuba <fsegura@fi.uba.ar>
2025-02-16 22:45:15 -0300

    arreglo el CRUD de consola y el problma con la migracion

--- backend/prisma/migrations/20241219131512_borro_consola_id/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `consolaId` on the `Juego` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Juego" DROP COLUMN "consolaId";
--- backend/prisma/migrations/20250216230924_agrego_consola_juegos_dlcs/migration.sql
+++ backend/prisma/migrations/20250216230924_agrego_consola_juegos_dlcs/migration.sql
@@ -5,8 +5,7 @@ CREATE TABLE "Juego" (
     "descripcion" TEXT NOT NULL,
     "modoDeJuegoId" INTEGER NOT NULL,
     "generoId" INTEGER NOT NULL,
-    "consolaId" INTEGER NOT NULL,
-    "fecha_publicacion" TIMESTAMP(3) NOT NULL,
+    "fecha_lanzamiento" TIMESTAMP(3) NOT NULL,
     "peso" DECIMAL(65,30) NOT NULL,
 
     CONSTRAINT "Juego_pkey" PRIMARY KEY ("id")
@@ -18,7 +17,7 @@ CREATE TABLE "DLC" (
     "juegoId" INTEGER NOT NULL,
     "titulo" TEXT NOT NULL,
     "descripcion" TEXT NOT NULL,
-    "fecha_publicacion" TIMESTAMP(3) NOT NULL,
+    "fecha_lanzamiento" TIMESTAMP(3) NOT NULL,
     "peso" DECIMAL(65,30) NOT NULL,
 
     CONSTRAINT "DLC_pkey" PRIMARY KEY ("id")
@@ -28,6 +27,10 @@ CREATE TABLE "DLC" (
 CREATE TABLE "Consola" (
     "id" SERIAL NOT NULL,
     "nombre" TEXT NOT NULL,
+    "fecha_lanzamiento" DATE,
+    "desarrollador" TEXT,
+    "almacenamiento" DECIMAL(65,30),
+    "tipo" TEXT NOT NULL,
 
     CONSTRAINT "Consola_pkey" PRIMARY KEY ("id")
 );
@@ -48,6 +51,17 @@ CREATE TABLE "Genero" (
     CONSTRAINT "Genero_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "_JuegoConsola" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL,
+
+    CONSTRAINT "_JuegoConsola_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateIndex
+CREATE INDEX "_JuegoConsola_B_index" ON "_JuegoConsola"("B");
+
 -- AddForeignKey
 ALTER TABLE "Juego" ADD CONSTRAINT "Juego_modoDeJuegoId_fkey" FOREIGN KEY ("modoDeJuegoId") REFERENCES "ModoDeJuego"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
@@ -55,7 +69,10 @@ ALTER TABLE "Juego" ADD CONSTRAINT "Juego_modoDeJuegoId_fkey" FOREIGN KEY ("modo
 ALTER TABLE "Juego" ADD CONSTRAINT "Juego_generoId_fkey" FOREIGN KEY ("generoId") REFERENCES "Genero"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Juego" ADD CONSTRAINT "Juego_consolaId_fkey" FOREIGN KEY ("consolaId") REFERENCES "Consola"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "DLC" ADD CONSTRAINT "DLC_juegoId_fkey" FOREIGN KEY ("juegoId") REFERENCES "Juego"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "DLC" ADD CONSTRAINT "DLC_juegoId_fkey" FOREIGN KEY ("juegoId") REFERENCES "Juego"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "_JuegoConsola" ADD CONSTRAINT "_JuegoConsola_A_fkey" FOREIGN KEY ("A") REFERENCES "Consola"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_JuegoConsola" ADD CONSTRAINT "_JuegoConsola_B_fkey" FOREIGN KEY ("B") REFERENCES "Juego"("id") ON DELETE CASCADE ON UPDATE CASCADE;

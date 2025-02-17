ffadabb0264811e8e26b16f772dcfc825806fe9e
Sofia <salvaradom@fi.uba.ar>
2025-02-17 16:36:39 -0300

    Arreglo error formulario juegos

--- frontend/js/getDataConsolas.js
+++ frontend/js/getDataConsolas.js
@@ -1,8 +1,9 @@
+document.addEventListener("DOMContentLoaded", getDataConsolas);
+
 function getDataConsolas () {
-    fetch('http://127.0.0.1:3000/api/v1/consolas')
+    fetch('http://localhost:3000/api/v1/consolas')
     .then(response => response.json())
     .then(json => {
         console.log(json);
     });
 };
-

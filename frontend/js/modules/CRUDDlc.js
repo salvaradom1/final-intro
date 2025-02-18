bbe3d42e8728228aad02815c7c0e0b5ba3275916
Sofia <salvaradom@fi.uba.ar>
2025-02-18 17:57:51 -0300

    /GET para DLC

--- frontend/js/modules/CRUDDlc.js
+++ frontend/js/modules/CRUDDlc.js
@@ -1,32 +1,30 @@
 document.addEventListener("DOMContentLoaded", getDataConsolas);
 
 function getDataConsolas() {
-    fetch('http://localhost:3000/api/v1/consolas')
+    fetch('http://localhost:3000/api/v1/dlcs')
         .then(response => response.json())
-        .then(consolas => {
-            console.log(consolas); 
-            let consolasContainer = document.getElementById('consolas-container');
-            consolasContainer.innerHTML = '';
+        .then(dlcs => {
+            console.log(dlcs); 
+            let dlcsContainer = document.getElementById('dlcscontainer');
+            dlcsContainer.innerHTML = '';
 
-            consolas.forEach(consola => {
+            dlcs.forEach(dlc => {
                 let card = `
                     <div class="col-sm-4">
                         <div class="card text-left border-0 shadow rounded-0 p-3" style="max-width: 22rem;">
                             <div class="icon">
                                 <svg class="icon bi d-block mx-auto mb-1" width="24" height="24" fill="currentColor">
-                                    <use xlink:href="node_modules/bootstrap-icons/bootstrap-icons.svg#display"></use>
+                                    <use xlink:href="node_modules/bootstrap-icons/bootstrap-icons.svg#puzzle"></use>
                                 </svg>
                             </div>
                             <div class="card-body">
-                                <h4 class="card-title text-center fw-bold text">${consola.nombre}</h4>
+                                <h4 class="card-title text-center fw-bold text">${dlc.titulo}</h4>
                                 <ul class="list-group list-group-flush">
-                                    <li class="list-group-item">Fecha de lanzamiento: ${consola.fecha_lanzamiento}</li>
-                                    <li class="list-group-item">Desarrollador: ${consola.desarrollador}</li>
-                                    <li class="list-group-item">Almacenamiento: ${consola.almacenamiento}</li>
-                                    <li class="list-group-item">Tipo: ${consola.tipo}</li>
-                                    <li class="list-group-item">Juegos: 
-                                        <button class="btn btn-link-light" onclick="mostrarJuegos(${consola.id})">Ver compatibles</button>
-                                    </li>
+                                    <li class="list-group-item">Juego: ${consola.juego}</li>
+                                    <li class="list-group-item">Descripcion: ${consola.descripcion}</li>
+                                    <li class="list-group-item">Fecha de lanzamiento: ${consola.fecha_lanzamiento}</p>
+                                    <li class="list-group-item">Peso: ${consola.peso}</li>
+                
                                 </ul>
                                 <div class="d-flex justify-content-start my-3"> 
                                     <button class="btn btn-light ms-3" type="button">
@@ -57,7 +55,7 @@ function getGamesForConsola(consolaId) {
 
 function mostrarJuegos(consolaId) {
     getGamesForConsola(consolaId).then(games => {
-        let gamesList = games.map(game => `<li>${game.titulo}</li>`).join('');
+        let gamesList = games.map(game => `<li>${game.nombre}</li>`).join('');
         document.getElementById('modal-games-list').innerHTML = `<ul>${gamesList}</ul>`;
    
         let modal = new bootstrap.Modal(document.getElementById('gamesModal'));

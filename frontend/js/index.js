4daae01a18ca3b6a85810f0f27bd609708947a84
Sofia <salvaradom@fi.uba.ar>
2025-02-19 22:34:53 -0300

    POST de consolas terminado

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,10 +1,7 @@
 import { limpiarConsola } from './modules/scriptForms.js'
-import { buscarJuegos } from './modules/scriptForms.js';
 import { createConsole } from './modules/scriptForms.js'; 
 
 limpiarConsola();
-buscarJuegos();
-
 document.getElementById("submit").addEventListener("click", createConsole);
 
 
